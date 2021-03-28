7ac79d14faa50debd96bf348438ed3e86b9dd5ed
andrioc <andrio@rodrigo.silva@hotmail.com>
2021-03-28 17:37:40 -0300

    Second section completed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,64 @@ import Header from '../../components/Header'
 
 import {
     Container,
+    Content,
+    ContentFirstSection,
+    ContentSecondSection
 } from './styles'
 
 const Home:React.FC = () => {
     return (
         <Container>
             <Header />
+
+            <Content>
+                <section>
+                    <ContentFirstSection>
+                        <div className={'firstSection-first-block'}>
+                            <h1>Designed for the future</h1>
+                        </div>
+
+                        <div className={'firstSection-second-block'}>
+                            <h1>Introducing an extensible editor</h1>
+                            <p>
+                                Blogr features an exceedingly intuitive interface which 
+                                lets you focus on one thing: creating content. The 
+                                editor supports management of multiple blogs and 
+                                allows easy manipulation of embeds such as images, videos, 
+                                and Markdown. Extensibility with plugins and themes 
+                                provide easy ways to add functionality or change the 
+                                looks of a blog.
+                            </p>
+                        </div>
+
+                        <div className={'firstSection-third-block'}>
+                            <h1>Robust content management</h1>
+                            <p>
+                                Flexible content management enables users to easily move through 
+                                posts. Increase the usability of your blog by adding customized 
+                                categories, sections, format, or flow. With this functionality, you’re 
+                                in full control.
+                            </p>
+                        </div>
+                    </ContentFirstSection>     
+                </section>
+
+                <section>
+                    <ContentSecondSection>
+                        <div>
+                            <div className='bg-phone-second-section' />
+                            <h1>State of the Art Infrastructure</h1>
+                            <p>
+                                With reliability and speed in mind, worldwide data centers provide the 
+                                backbone for ultra-fast connectivity. This ensures your site will load 
+                                instantly, no matter where your readers are, keeping your site competitive.    
+                            </p>
+                        </div>
+                    </ContentSecondSection>
+                </section>
+
+
+            </Content>
         </Container>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,157 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+import imgBgSectionOne from '../../assets/images/illustration-editor-mobile.svg'
+import imgPhoneBgSectionTwo from '../../assets/images/illustration-phones.svg'
+import imgCirclesBgSectionTwo from '../../assets/images/bg-pattern-circles.svg'
 
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    margin-top: 100px;
+
+    @media(max-width: 900px){
+        section {
+            width: 100%;
+        }
+    }
+
+
+`;
+
+export const ContentFirstSection = styled.div`
+    display: flex;
+    
+    @media(max-width: 900px){
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+       
+        background: url(${imgBgSectionOne}) no-repeat;
+        background-position: top 60px center;
+
+        .firstSection-first-block{
+            h1 {
+                font-size: 28px;
+                color: var(--blue-400);
+                font-weight: 600;
+            }
+        }
+
+        .firstSection-second-block{
+            margin-top: 390px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            h1 {
+                font-size: 28px;
+                color: var(--blue-400);
+                font-weight: 600;
+                max-width: 240px;
+                text-align: center;
+            }
+
+            p {
+                max-width: 300px;
+                text-align: center;
+                margin-top: 16px;
+                font-weight: 300;
+                line-height: 27px;
+                color: var(--gray-300);
+            }
+
+        }
+
+        .firstSection-third-block{
+            margin-top: 40px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            h1 {
+                font-size: 28px;
+                color: var(--blue-400);
+                font-weight: 600;
+                max-width: 200px;
+                text-align: center;
+            }
+
+            p {
+                max-width: 300px;
+                text-align: center;
+                margin-top: 16px;
+                font-weight: 300;
+                line-height: 27px;
+                color: var(--gray-300);
+            }
+        }
+    }
+`;
+
+export const ContentSecondSection = styled.div`
+    
+
+    @media(max-width: 900px){
+
+        z-index: 999;
+        margin-top: 270px;
+        display: flex;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+
+        > div {
+            display: flex;
+            width: 100%;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            overflow: visible;
+            background: url(${imgCirclesBgSectionTwo}) no-repeat, linear-gradient(135deg, var(--blue-950) 0%, var(--blue-100) 100%);
+            background-position: bottom 180px center, 0 0;
+            background-size: 800px;
+            border-radius: 0px 100px;
+            z-index: 1;
+            height: 624px;
+            position: relative;
+
+            .bg-phone-second-section{
+                height: 420px;
+                width: 400px;
+                position: absolute;
+                background: url(${imgPhoneBgSectionTwo}) no-repeat;
+                background-position: center;
+                background-size: cover;
+                top: -210px;
+            }
+
+            h1 {
+                font-size: 40px;
+                font-weight: 600;
+                color: var(--white);    
+                text-align: center;
+                margin-top: 180px;
+            }
+
+            p {
+                margin-top: 12px;
+                text-align: center;
+                font-size: 16px;
+                line-height: 27px;
+                color: var(--white);
+                font-weight: normal;
+                max-width: 300px;
+            }
+        }
+    }
 
 `;
